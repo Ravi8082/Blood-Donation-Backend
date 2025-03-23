63bc492444bb29b9ec1dcdb6939f0ca4ec53eead
Ravi Pal <palravi1093@gmail.com>
2025-03-23 18:16:33 +0530

    Update Request.java

--- src/main/java/com/example/demo/Entity/Request.java
+++ src/main/java/com/example/demo/Entity/Request.java
@@ -2,13 +2,11 @@ package com.example.demo.Entity;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;  // Correct import for JPA
 import javax.validation.constraints.*;
-
 
 @Entity
 @Getter

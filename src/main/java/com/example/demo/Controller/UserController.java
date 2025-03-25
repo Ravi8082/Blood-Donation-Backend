bca22649e0bf3b41bce9dfc1204516cafeaadaa0
Ravi Pal <palravi1093@gmail.com>
2025-03-25 16:10:30 +0530

    Update UserController.java

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -23,7 +23,7 @@ import java.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://blood-donation-frontended-7.onrender.com")
 public class UserController {
 
     @Autowired
